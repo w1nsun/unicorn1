f0ccf682c44b7d94e267f351620b5f659b6d68e8
w1nsun <ilya.nevmyvaka@gmail.com>
2022-02-12 23:14:57 +0200

    init2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AccountModule } from './account/account.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: process.env.NODE_ENV
+                ? `.${process.env.NODE_ENV}.env`
+                : '.env',
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+            synchronize: true,
+            host: process.env.POSTGRES_HOST,
+            entities: [],
+        }),
+        AccountModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
-
