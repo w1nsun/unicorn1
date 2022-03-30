9a8d23ac53942950c8cbd513107acacc3376f76f
w1nsun <ilya.nevmyvaka@gmail.com>
2022-03-30 15:38:25 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Beauty CRM')
+        .setDescription('Beauty CRM API description')
+        .setVersion('1.0')
+        .addTag('beauty')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(PORT);
 }
