e59f23869a95538880034e1632edf7a72c3aa360
w1nsun <ilya.nevmyvaka@gmail.com>
2022-04-22 12:40:42 +0300

    add fixtures for chain

---
+++ src/chain/fixture/chain.fixture.ts
@@ -0,0 +1,32 @@
+import { Connection } from 'typeorm';
+import { UuidService } from '../../core/service/uuid.service';
+import { Chain } from '../entity/chain.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ChainFixture {
+    constructor(
+        private connection: Connection,
+        private uuidService: UuidService,
+    ) {}
+
+    async load() {
+        console.log('__before load');
+        const res = await this.connection
+            .createQueryBuilder()
+            .insert()
+            .into(Chain)
+            .values([
+                {
+                    id: this.uuidService.generateV4(),
+                    title: 'Test from fixture',
+                    active: true,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                },
+            ])
+            .execute();
+
+        console.log(res);
+    }
+}
--- src/console.ts
+++ src/console.ts
@@ -1,6 +1,7 @@
 //https://stackoverflow.com/questions/65250657/using-services-inside-nestjs-script-run-from-command-line
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ChainFixture } from './chain/fixture/chain.fixture';
 
 async function bootstrap() {
     const application = await NestFactory.createApplicationContext(AppModule);
@@ -8,8 +9,11 @@ async function bootstrap() {
     const command = process.argv[2];
 
     switch (command) {
-        case 'create-administrator-user':
-            console.log('create-administrator-user');
+        case 'fixtures-load':
+            const chainFixture: ChainFixture = application.get(ChainFixture);
+
+            await chainFixture.load();
+            console.log('Loaded');
             break;
         default:
             console.log('Command not found');
