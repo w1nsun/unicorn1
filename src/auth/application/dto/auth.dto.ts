b3daa9746b276169a7ab4e1c2bd0cecb9c6c6fa2
w1nsun <ilya.nevmyvaka@gmail.com>
2022-06-13 23:45:57 +0200

    auth repo

--- src/auth/application/dto/auth.dto.ts
+++ src/auth/application/dto/auth.dto.ts
@@ -3,10 +3,14 @@ import { Auth } from '@auth/domain/entity/auth.entity';
 
 export class AuthDto {
     @ApiProperty()
+    id: string;
+
+    @ApiProperty()
     logins: string[];
 
     static fromEntity(entity: Auth): AuthDto {
         const dto = new this();
+        dto.id = entity.id;
         dto.logins = entity.logins.map((value) => {
             return value.login;
         });
